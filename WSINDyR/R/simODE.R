c3122c61297af33df3bf854590264b4c48efd45e
Derek Goulet <djg1034@usnh.edu>
2023-04-15 13:02:58 -0600

    Finish R1 SimODE

--- WSINDyR/R/simODE.R
+++ WSINDyR/R/simODE.R
@@ -1,5 +1,9 @@
 #simODE - gets ode simulations
 
+
+install.packages('deSolve')
+library(deSolve) #this allows us to solve IVPS
+
 simODE <- function(x0, t_span, t_eval, tol_ode, ode_name, params,
                    noise_ratio) {
 
@@ -17,6 +21,7 @@ simODE <- function(x0, t_span, t_eval, tol_ode, ode_name, params,
 
 
   } else if (ode_name == 'Logistic_Growth') {
+
     pow <- 2
     rhs <- function(t,x) {
       return(x - x^pow)
@@ -30,30 +35,71 @@ simODE <- function(x0, t_span, t_eval, tol_ode, ode_name, params,
       return(c(x[2], mu*x[2]-mu*x[1]^2*x[2]-x[1]))
     }
 
+    datas <- c(0,1,1, 1, 0, -1, 0, 1, mu, 2, 1, -mu)
+    weights = matrix(datas, nrow = 4, ncol = 3, byrow = TRUE)
 
+  w} else if (ode_name == 'Duffing') {
 
-  } else if (ode_name == 'Duffing') {
-    mu <- 0.2
-    ode_params <- c(mu, mu^2/(4*5), 1)
-    x0 <- t(c(0,2)) #np.array([3,0]).T
-    t_span <- c(0, 30)
-    t_eval <- seq(0, 30, 0.01)
+    mu <- params[1]
+    alpha <- params[2]
+    beta <- params[3]
+
+    rhs <- function(t,x) {
+      return(c(x[2], -mu*x[2]-alpha*x[1]-beta*x[1]^3))
+    }
+
+    datas <- c(0,1,1, 1, 0, -alpha, 0, 1, -mu, 3, 0, -beta)
+    weights = matrix(datas, nrow = 4, ncol = 3, byrow = TRUE)
+
 
   } else if (ode_name == 'Lotka_Volterra') {
-    alpha <- 2/3
-    beta <- 4/3
-    ode_params <- c(alpha, beta, 1, 1)
-    x0 <- t(c(10,10)) #np.array([3,0]).T
-    t_span <- c(0, 200)
-    t_eval <- seq(0, 200, 0.02)
+
+    alpha <- params[1]
+    beta <- params[2]
+    delta <- params[3]
+    gamma <- params[4]
+
+    rhs <- function(t,x) {
+      reutrn(c(alpha*x[1]-beta*x[1]*x[2], delta*x[1]*x[2] - gamma*x[2]))
+    }
+
+    datas <- c(1, 0, alpha, 1, 1, -beta, 0, 1, -gamma, 1, 1, delta)
+    weights <- matrix(datas, nrow = 4, ncol = 4, byrow = TRUE)
 
   } else if (ode_name == 'Lorenz') {
-    ode_params <- c(10, 8/3, 27)
-    x0 <- t(c(-8, 10, 27)) #np.array([3,0]).T
-    t_span <- c(0.001, 10)
-    t_eval <- linspace(0.001, 10, 5000)
+
+    sigma <- params[1]
+    beta <- params[2]
+    rho <- params[3]
+
+    rhs <- function(t,x) {
+      return(lorenz(x, sigma, beta, rho))
+    }
+
+    datas <- c(0, 1, 0, sigma,
+               1, 0, 0, -sigma,
+               1, 0, 0, rho,
+               1, 0, 1, -1,
+               0, 1, 0, -1,
+               1, 1, 0, 1,
+               0, 0, 1, -beta)
+
+    weights <- matrix(datas, nrow= 7, ncol = 4, byrow = TRUE)
 
   } else {
     disp('No ODE selected')
   }
+
+  sol <- ode(y = x0, times = t_eval, func = rhs, parms = ode_params)  #UNSURE what to call for parameters
+
+  x <- t(sol[,"Y"])
+  xobs <- addNoise(x, noise_ratio)
+
+  anslist <- list('weights' = weights,
+                  'sol.t' = t(sol),
+                  'xobs' = xobs,
+                  'rhs' = rhs)
+
+  return(anslist)
+
 }
