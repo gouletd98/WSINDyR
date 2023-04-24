28eb721023ee76d66961f1481247d0cf8d07ac53
Adam Manaster <adma9806@colorado.edu>
2023-04-24 12:46:16 -0600

    Edited tutorial script

---
+++ WSINDyR/R/Tutorial.R
@@ -0,0 +1,24 @@
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+test <- function(x, y, z) {
+  result <- vector(mode = "numeric", length = length(x))
+  for (i in seq_along(x)) {
+
+    if (y[i] < z) {
+      result[i] <- x[i] + y[i]
+
+    } else {
+      result[i] <- x[i] - y[i]
+
+    }
+  }
+  return(result)
+}
