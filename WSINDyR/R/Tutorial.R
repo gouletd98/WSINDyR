4ef1169faf0c5b6931021e96a39cf20eeeb51c42
Adam Manaster <adma9806@colorado.edu>
2023-04-25 13:21:49 -0600

    Edited Tutorial.R some more

--- WSINDyR/R/Tutorial.R
+++ WSINDyR/R/Tutorial.R
@@ -1,16 +1,10 @@
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+### Tutorial.R (APPM 4720/5720)
+### Adam Manaster and Derek Goulet
+### Last Edit on 04/25/2023
 
-x <- as.vector(1, 3, 5, 7, 9)
-y <- as.vector(2, 4, 6, 8, 10)
-z <- 5
+# Basic Function ----------------------------------------------------------
+
+# define function `test` below
 
 test <- function(x, y, z) {
   result <- vector(mode = "numeric", length = length(x))
@@ -27,6 +21,62 @@ test <- function(x, y, z) {
   return(result)
 }
 
-### lstsq() example
+# intialize variables - concatenating `x0` and `y0` into vectors
+x0 <- c(1, 3, 5, 7, 9)
+y0 <- c(2, 4, 6, 8, 10)
+z0 <- 5
 
-### qr.solve() example - sparsifyDynamics.R
+# run the function and view result
+test(x = x0, y = y0, z = z0)
+
+
+
+# Data Frame --------------------------------------------------------------
+
+# can read in '.csv' files from URL (or locally), as well as HTML tables online (although might require some labor)
+df_iris <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header = FALSE)
+
+# check out first few rows of `df_iris`
+head(df_iris)
+
+# get values in 10th row
+df_iris[10,]
+
+# get values in 5th column
+df_iris$V5 # or ... `df_iris[5]`
+
+
+
+# Example w/ `lstsq()` and Plotting ---------------------------------------
+
+### finds the coefficients of a linear equation that best fits a set of data points
+
+# load in `ggplot2` to demonstrate its plotting capabilities
+library(ggplot2)
+library(prama)
+
+# generate sample data
+x <- 1:10
+y <- 3 * x + rnorm(10)
+
+# create data frame with the x and y values
+df <- data.frame(x = x, y = y)
+
+# use `lstsq()`` to fit a linear model
+X <- cbind(1, x)
+b <- lstsq(X, y)
+predicted_y <- X %*% b
+
+# plot the data using ggplot2
+ggplot(data = df, aes(x = x, y = y)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  geom_line(aes(y = predicted_y), linetype = "dashed") +
+  labs(title = "Linear Regression", x = "x", y = "y")
+
+
+
+# Example w/ `qr.solve()` -------------------------------------------------
+
+# sparsifyDynamics.R
+
